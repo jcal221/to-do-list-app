0fbf56ed994e232336dd1290deef320cf0a11511
jcal221 <115591229+jcal221@users.noreply.github.com>
2023-06-03 23:15:51 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,31 @@
 $(document).ready(function() {
-    function newItem() {
-      let inputValue = $('#input').val();
-      if (inputValue === '') {
-        alert("You must write something!");
-        return;
-      }
-  
-      let li = '<li>' + inputValue + '</li>';
-      $('#list').append(li);
-  
-      $('#input').val('');
-  
-      $(li).on('dblclick', function() {
-        $(this).toggleClass('strike');
-      });
-  
-      let deleteButton = $('<crossOutButton></crossOutButton>');
-      deleteButton.text('X');
-      $(li).append(deleteButton);
-  
-      deleteButton.on('click', function() {
-        $(li).remove();
-      });
-  
-      $('#list').sortable();
+  function newItem() {
+    let inputValue = $('#input').val();
+    if (inputValue === '') {
+      alert("You must write something!");
+      return;
     }
-  
-    $('#button').on('click', function() {
-      newItem();
+
+    let li = $('<li>' + inputValue + '</li>');
+    $('#list').append(li);
+
+    $('#input').val('');
+
+    li.on('dblclick', function() {
+      $(this).toggleClass('strike');
     });
+
+    let deleteButton = $('<button class="crossOutButton">X</button>');
+    li.append(deleteButton);
+
+    deleteButton.on('click', function() {
+      $(this).parent().remove();
+    });
+
+    $('#list').sortable();
+  }
+
+  $('#button').on('click', function() {
+    newItem();
   });
-  
+});
